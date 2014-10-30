285ba0c4b98a5674afbc70916c7c482de0a2de46
SeedPaths-User <lessdismissive@email.com>
2014-10-29 20:49:43 -0600

    10/29/2014

---
+++ FlipMania/FlipMania/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoins(10);
+            FlipForHeads(10000);
+
+            Console.ReadKey();
+        }
+        static void FlipCoins(int numberOfFlips)
+        {
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+            Random coinFlip= new Random();
+            
+            for( int i= 0; i <= numberOfFlips; i++)
+            {
+           int randomNumber= coinFlip.Next(0,2);
+           if (randomNumber==1)
+           {
+               numberOfTails = numberOfTails + 1;
+           }
+           else {
+               numberOfHeads = numberOfHeads + 1; }
+           
+           }Console.WriteLine("We flipped a coin " + numberOfFlips + " times.");
+           Console.WriteLine("Number of Heads: " + numberOfHeads);
+           Console.WriteLine("Number of Tails: " + numberOfTails);
+            
+Console.ReadKey(); 
+        }
+        static void FlipForHeads(int numberOfHeads)
+        {
+            int numberOfHeadsFlipped = 0;
+            int totalFlips = 0;
+            Random coinFlip = new Random();
+            
+            while (numberOfHeadsFlipped <= numberOfHeads)
+            {
+                int randomNumber = coinFlip.Next(0, 2);
+                    totalFlips= totalFlips+1;
+                    if (randomNumber == 1)
+                    {
+                        numberOfHeadsFlipped = numberOfHeadsFlipped + 1;
+                    }
+                
+
+            }Console.WriteLine("We are flipping a coin until we find " + numberOfHeads + " heads");
+                Console.WriteLine("It took " + totalFlips + " flips to find " + numberOfHeads + " heads");
+            Console.ReadKey();
+        } 
+
+    } 
+} 
+
---
+++ fizzbuzzpractice/fizzbuzzpractice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzzpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fizzBuzz(1, 30);
+            lettersInWord("e" , "Kevin");
+            Console.WriteLine();
+            Console.ReadKey();
+          
+        }
+        static void fizzBuzz(int startNum, int endNum){
+          for(int i= startNum; i <=endNum; i++){
+              if (i % 3 ==0 && i % 5 ==0){
+                  Console.WriteLine("FizzBuzz");
+                  
+              }
+              else if (i % 3==0){
+                  Console.WriteLine("Fizz");
+              }
+              else if (i % 5==0){
+                  Console.WriteLine("Buzz");
+              }
+              else Console.WriteLine(i);
+          }
+
+            Console.ReadKey();
+           
+        }
+        static void lettersInWord(string letterLookedFor, string word)
+        {
+            int letterCount = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                //the index will find a letter in the word
+                // it comes as a char, since its only 1 character
+                //the .tostring converts it to a string
+                //.tolower makes it lower case
+                string letter = word[i].ToString().ToLower();
+                if (letter == letterLookedFor)
+                {
+                    letterCount = letterCount + 1;
+                    
+
+                }
+                
+                }Console.WriteLine(word + " has " + letterCount + " " +  letterLookedFor + " in it.");
+                Console.ReadKey();
+            }
+        }
+    }
+   
+
+    
+
